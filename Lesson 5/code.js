8af8c03fd1bcee3f4b64b692d7c5d354d910ef89
Kostya <Kpstya@mail.ru>
2020-05-08 01:16:10 +0300

    lesson5

---
+++ Lesson 5/code.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'img/oops.jpg',
+        searchLine: '',
+        cart: 'cart-block',
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+        },
+        filterGoods() {
+
+        },
+        showCart(){
+
+        }
+
+    },
+    mounted() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    },
+});
